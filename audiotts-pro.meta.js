4990f5e5e9b7a7aea19d60daebd098e0a71a10fc
afkarxyz <muhammadzamzamafkar@gmail.com>
2025-11-07 07:20:50 +0700

    .

---
+++ audiotts-pro.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            AudioTTS Pro | Text-to-Speech Generator & Audio Downloader
+// @description     Text-to-Speech generator and audio downloader supporting Azure AI Speech, IBM Watson, and Google Gemini TTS.
+// @namespace       https://exyezed.cc
+// @version         1.0.1
+// @author          exyezed
+// @license         MIT
+// @supportURL      https://exyezed.cc
+// @homepageURL     https://exyezed.cc
+// @updateURL       https://github.com/exyezed/audiotts-pro/releases/latest/download/audiotts-pro.meta.js
+// @downloadURL     https://github.com/exyezed/audiotts-pro/releases/latest/download/audiotts-pro.user.js
+// ==/UserScript==
